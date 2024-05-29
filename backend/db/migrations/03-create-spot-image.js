33c0057da06746ad697c222d6281f405ef0f5a8d
Michel Garcia Ribalta <149029907+MyNameIsJeff-305@users.noreply.github.com>
2024-05-29 18:51:20 -0400

    Minor Changes

--- backend/db/migrations/03-create-spot-image.js
+++ backend/db/migrations/03-create-spot-image.js
@@ -25,7 +25,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: "Spots",
+          model: 'Spots',
           key: 'id',
           // onDelete: 'CASCADE'
         },
