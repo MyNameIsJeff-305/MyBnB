dbeb6bde23503729b102ff4c560a3370072bffa4
Michel <michelgarcia950528@icloud.com>
2024-05-31 14:34:46 -0400

    Debugging 'title' and 'stack' errors 2

--- backend/app.js
+++ backend/app.js
@@ -1,74 +1,90 @@
+// Imports -- External
 const express = require('express');
-require('express-async-errors');
 const morgan = require('morgan');
 const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const { ValidationError } = require('sequelize');
+require('express-async-errors');
 
-const routes = require('./routes');
+// Imports -- Internal
 const { environment } = require('./config');
+const routes = require('./routes');
+
+
 const isProduction = environment === 'production';
 
+// Initialize app
 const app = express();
-app.use(morgan('dev'));
+
+// use all global middleware
+app.use(morgan('dev'));  // logging
+
+// converts json req bodies into parsed objects attached to req.body
 app.use(cookieParser());
 app.use(express.json());
 
-
-//Security Middleware
+// Security Middleware
 if (!isProduction) {
-    app.use(cors()); //enable cors in development exclusively
+    app.use(cors());
 }
 
-app.use(
-    helmet.crossOriginResourcePolicy({
-        policy: 'cross-origin'
-    })
+app.use(helmet.crossOriginResourcePolicy({
+    policy: "cross-origin"
+})
 );
 
-//_csrf token and req.csrfToken
 app.use(
     csurf({
         cookie: {
             secure: isProduction,
-            sameSite: isProduction && 'Lax',
+            sameSite: isProduction && "Lax",
             httpOnly: true
         }
-    }));
+    })
+);
 
-app.use(routes); //API routes
+// Routes
+app.use(routes);
 
-//Catch unhandled requests and orward to error handler.
-// app.use((_req, _res, next) => {
-//     const err = new Error("The requested resource couldn't be found");
-//     err.title = "Resource not Found";
-//     err.errors = { message: "The Requested resource couldn't be found" };
-//     err.status = 404;
-//     next(err);
-// });
+// Error handling middleware
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = { message: "The requested resource couldn't be found." };
+    err.status = 404;
+    next(err.errors);
+});
 
-//Process Sequelize errors
 app.use((err, _req, _res, next) => {
+    console.log(err.errors, "we are here")
     // check if error is a Sequelize error:
     if (err instanceof ValidationError) {
-      err.errors = err.errors.map((error) => error.message);
-      err.title = 'Validation error';
+        let errors = {};
+        for (let error of err.errors) {
+            errors[error.path] = error.message;
+        }
+        err.title = 'Validation error';
+        err.errors = errors;
     }
     next(err);
-  });
+});
 
-//Error Formatter
-app.use((err, _req, res, _next) => {
+app.use((err, _req, res, _next) => {  // error formatter
     res.status(err.status || 500);
+    if (isProduction) {
+        delete err.title;
+        delete err.stack;
+    }
     console.error(err);
     res.json({
-        title: err.title || 'Server Error',
+        title: err.title,
         message: err.message,
         errors: err.errors,
-        stack: isProduction ? null : err.stack
+        stack: err.stack   // edit later
     });
 });
 
+// Exports
 module.exports = app;
