1204635ab6e95f660b8a7fb72acdf8423159303d
Michel Garcia Ribalta <149029907+MyNameIsJeff-305@users.noreply.github.com>
2024-06-26 19:53:30 -0400

    Redux Store Setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { Provider } from 'react-redux';
+import configureStore from './store';
+
+const store = configureStore();
+
+if (import.meta.env.MODE !== 'production') {
+  window.store = store;
+  console.log(store);
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
+
