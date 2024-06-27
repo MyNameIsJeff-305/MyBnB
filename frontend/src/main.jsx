123e5b48059b04f2875f9ea39268e558947b4e41
Michel Garcia Ribalta <149029907+MyNameIsJeff-305@users.noreply.github.com>
2024-06-26 20:17:27 -0400

    Phase 0 Done

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,11 +5,15 @@ import './index.css';
 import { Provider } from 'react-redux';
 import configureStore from './store';
 
+import { restoreCSRF, csrfFetch } from './store/csrf';
+
 const store = configureStore();
 
 if (import.meta.env.MODE !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
-  console.log(store);
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
