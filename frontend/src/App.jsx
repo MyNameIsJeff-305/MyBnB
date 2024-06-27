0723621ed3d9482089fe97aa36332963308c9d93
Michel Garcia Ribalta <149029907+MyNameIsJeff-305@users.noreply.github.com>
2024-06-26 23:31:48 -0400

    Login Form Component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,21 @@
+import LoginFormPage from './components/LoginFormPage';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Welcome! :)</div>
+  },
+  {
+    path: '/login',
+    element: <LoginFormPage />,
+  },
+]);
+
 function App() {
-  return <h1> Hello from App </h1>;
-}
+  return (
+    <RouterProvider router={router} />
+  )
+};
 
 export default App;
---
+++ frontend/src/components/LoginFormPage/LoginFormPage.jsx
@@ -0,0 +1,67 @@
+// frontend/src/components/LoginForm.js
+
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import * as sessionActions from '../../store/session';
+import { Navigate } from 'react-router-dom';
+
+import './LoginForm.css';
+
+const LoginFormPage = () => {
+    const dispatch = useDispatch();
+    const sessionUser = useSelector((state) => state.session.user);
+    const [credential, setCredential] = useState('');
+    const [password, setPassword] = useState('');
+    const [validationErrors, setValidationErrors] = useState({});
+
+    if (sessionUser) return <Navigate to='/' replace={true} />;
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setValidationErrors({});
+        return dispatch(sessionActions.login({ credential, password })).catch(
+            async (res) => {
+                const data = await res.json();
+                if (data && data.errors) setValidationErrors(data.errors);
+            }
+        );
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+          <ul>
+            {validationErrors.errors ? <p>{validationErrors.errors}</p> : ''}
+          </ul>
+          <div className='login-items'>
+    
+            <label id='userName-field'>
+              Username or Email
+              <input
+                type="text"
+                className='login-box'
+                value={credential}
+                onChange={(e) => setCredential(e.target.value)}
+                required
+              />
+            </label>
+          </div>
+          <div className='login-items'>
+            <label id='password-field'>
+              Password
+              <input
+                type="password"
+                className='login-box'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </label>
+          </div>
+          <div className='login-items'>
+            <button type="submit" id='login-button'>Log In</button>
+          </div>
+        </form>
+      );
+    }
+
+export default LoginFormPage;
