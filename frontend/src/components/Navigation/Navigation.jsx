66651ca9fe6a74c620e387fdb1a2c8225c5bb42f
Michel Garcia Ribalta <149029907+MyNameIsJeff-305@users.noreply.github.com>
2024-06-27 23:38:25 -0400

    Profile Button Functionality Added

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,41 +1,21 @@
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from '../ProfileButton';
-import OpenModalButton from '../OpenModalButton';
-import LoginFormModal from '../LoginFormModal';
-import SignupFormModal from '../SignupFormModal';
 import './Navigation.css';
 
 function Navigation({ isLoaded }) {
-    const sessionUser = useSelector((state) => state.session.user);
-
-    const sessionLinks = sessionUser ? (
-        <li>
-            <ProfileButton user={sessionUser} />
-        </li>
-    ) : (
-        <>
-            <li>
-                <OpenModalButton
-                    buttonText="Log In"
-                    modalComponent={<LoginFormModal />}
-                />
-            </li>
-            <li>
-                <OpenModalButton
-                    buttonText="Sign Up"
-                    modalComponent={<SignupFormModal />}
-                />
-            </li>
-        </>
-    );
+    const sessionUser = useSelector(state => state.session.user);
 
     return (
         <ul className="navigation-list">
             <li>
                 <NavLink to="/">Home</NavLink>
             </li>
-            {isLoaded && sessionLinks}
+            {isLoaded && (
+                <li>
+                    <ProfileButton user={sessionUser} />
+                </li>
+            )}
         </ul>
     );
 }
