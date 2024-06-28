0c5c6df3afad86fc988eb45dabb1afe26ba174bc
Michel Garcia Ribalta <149029907+MyNameIsJeff-305@users.noreply.github.com>
2024-06-27 23:45:26 -0400

    Bonus Phase Done

---
+++ frontend/src/components/Navigation/OpenModalMenuItem.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useModal } from '../../context/Modal';
+import './Navigation.css'
+
+function OpenModalMenuItem({
+    modalComponent, // component to render inside the modal
+    itemText, // text of the menu item that opens the modal
+    onItemClick, // optional: callback function that will be called once the menu item that opens the modal is clicked
+    onModalClose // optional: callback function that will be called once the modal is closed
+}) {
+    const { setModalContent, setOnModalClose } = useModal();
+
+    const onClick = () => {
+        if (onModalClose) setOnModalClose(onModalClose);
+        setModalContent(modalComponent);
+        if (typeof onItemClick === "function") onItemClick();
+    };
+
+    return (
+        <div className='item'>
+            <li onClick={onClick}>{itemText}</li>
+        </div>
+    );
+}
+
+export default OpenModalMenuItem;
