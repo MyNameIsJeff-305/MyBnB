c630c2e49fbb867fefc375a310ad65c1d779bf9b
Michel Garcia Ribalta <149029907+MyNameIsJeff-305@users.noreply.github.com>
2024-07-17 22:23:16 -0400

    Final Styling

--- frontend/src/components/ImageSlider/ImageSlider.jsx
+++ frontend/src/components/ImageSlider/ImageSlider.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { FaAngleRight, FaAngleLeft } from "react-icons/fa6";
 import './ImageSlider.css';
 
 function ImageSlider({ images }) {
@@ -18,21 +19,17 @@ function ImageSlider({ images }) {
 
     return (
         <div className="image-slider">
-            <button onClick={prevImage} aria-label="Previous Image" className="slider-button">
-                <svg viewBox="0 0 24 24">
-                    <path d="M15.41 16.58L10.83 12l4.58-4.59L14 6l-6 6 6 6z"/>
-                </svg>
+            <button onClick={prevImage} className="slider-button">
+                <FaAngleLeft />
             </button>
-            <img 
-                src={images[currentImageIndex].url} 
-                alt={`Slide ${currentImageIndex + 1}`} 
-                className="slider-image" 
-                loading="lazy" 
+            <img
+                src={images[currentImageIndex].url}
+                alt={`Slide ${currentImageIndex + 1}`}
+                className="slider-image"
+                loading="lazy"
             />
-            <button onClick={nextImage} aria-label="Next Image" className="slider-button">
-                <svg viewBox="0 0 24 24">
-                    <path d="M8.59 16.58L13.17 12 8.59 7.41 10 6l6 6-6 6z"/>
-                </svg>
+            <button onClick={nextImage} className="slider-button">
+                <FaAngleRight />
             </button>
         </div>
     );
